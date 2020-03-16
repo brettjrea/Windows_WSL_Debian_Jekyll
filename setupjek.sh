5b0d1213f4bc6dcf2db3fe327ff68d30ac62ead4
brettjrea <6890798+brettjrea@users.noreply.github.com>
2020-03-16 00:09:09 -0400

    Update setupjek.sh

--- setupjek.sh
+++ setupjek.sh
@@ -6,9 +6,9 @@ sudo apt autoremove -y
 ### Install common packages.
 sudo apt install build-essential curl ruby -y
 ### Check ruby for updates.
-gem update --system
+sudo gem update --system
 ### Install Bundler Jekyll
-gem install bundler jekyll
+sudo gem install bundler jekyll
 ### Create jekyll site.
 jekyll new my-awesome-site
 ### Change directory.
